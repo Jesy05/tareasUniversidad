1bf493044deecfb024f569a7ae6780f93ed79ada
Jesy05 <jgonzalezj@uamv.edu.ni>
2025-05-16 10:07:39 -0600

    el for para los rangos esta mal hecho 🗣️

--- Multiplicar.py
+++ Multiplicar.py
@@ -2,21 +2,23 @@
 Autor: Jesy Nicole González Jarquín
 Fecha: 02/05/2025
 Versión: 1.0 
-Descripción: Este programa calcula la tabla de multiplicar del numero n 
-             desde 1 - m. 
+Descripción: Este programa calcula las tablas de multiplicar hasta el numero n 
+ingresado por el usuario. 
 '''
 import os
 
 def multiplicar():
     os.system("cls")
     print("📋 Tabla de Multiplicación")
-    n= int(input("Digite el número base de la tabla: "))
-    m= int(input("Digite el número hasta donde quiere multiplicar: "))
+    n= int(input("Digite el número de tablas de multiplicar que quiere: "))
+    m= int(input("Digite el número hasta el cual quiere multiplicar las tablas: "))
     print(" ")
-    print("La tabla de multiplicar del número", n, "es:")
-    for i in range(1, m+1):
-        print(n, "x", i, "=", n*i)
-    print(" ")
+    for i in range(1, n+1):
+        print("La tabla de multiplicar del número", i, "hasta el", m,"es:")
+        for j in range(1, m+1): # el 1 es el primer número a multiplicar y el m es el último número a multiplicar
+            # + 1 porque el rango no incluye el último número
+            print(i, "x", j, "=", i*j)
+        print(" ")
     print("¿Desea continuar? (s/n)")
     continuar = input("Digite su respuesta: ")
     if continuar.lower() == "s":
@@ -28,3 +30,5 @@ def multiplicar():
 
 if __name__ == "__main__":
     multiplicar()
+
+
