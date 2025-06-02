9ca1a1cd6d3636bccc3af9b6329499efda020f09
Jesy05 <jgonzalezj@uamv.edu.ni>
2025-06-02 10:42:38 -0600

    codigo para lo de branch

---
+++ archivo_parapracticarmerge.py
@@ -0,0 +1,22 @@
+# este codigo sirve para mostrar poner una contraseña y validar que es la correcta 
+
+import os
+
+def password():
+    os.system('cls') 
+    password = "contra123"
+    intentos = 3
+    while intentos > 0:
+        entrada = input("Introduce la contraseña: ")
+        if entrada == password:
+            print("Contraseña correcta.")
+            print("Puede pasarle adelante\n...\n...\n...")
+            return True
+        else:
+            intentos -= 1
+            print(f"Contraseña incorrecta. Te quedan {intentos} intentos.")
+    print("Has agotado todos los intentos.")
+    return False
+
+if __name__ == "__main__":
+    password()
