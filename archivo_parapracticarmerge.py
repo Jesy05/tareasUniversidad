3331110138f8cf51d975db2d06921da5ceecd2d1
Jesy05 <jgonzalezj@uamv.edu.ni>
2025-06-02 10:43:58 -0600

    alo

--- archivo_parapracticarmerge.py
+++ archivo_parapracticarmerge.py
@@ -5,7 +5,7 @@ import os
 def password():
     os.system('cls') 
     password = "contra123"
-    intentos = 3
+    intentos = 3 #tralala
     while intentos > 0:
         entrada = input("Introduce la contraseña: ")
         if entrada == password:
